2ebeb30543c6b7b00c558dcb4179d78a99d16207
Cyclone77 <fillchar@live.com>
2017-12-27 11:25:24 +0800

    增删改查

--- theme-app/src/app/components/tables/tables.component.ts
+++ theme-app/src/app/components/tables/tables.component.ts
@@ -16,39 +16,72 @@ export class TablesComponent implements OnInit {
   ELEMENT_DATA: Person[] = [];
   displayedColumns = ['position', 'name', 'weight', 'symbol', 'option'];
   dataSource = new MatTableDataSource();
-  psnData = { };
-
+  psnData: Person = {} as Person;
+  myHttpHead = { headers: new HttpHeaders({ 'Content-Type': 'application/json' }) };
   @ViewChild(MatPaginator) paginator: MatPaginator;
   constructor(
     private http: HttpClient
   ) { }
 
   ngOnInit() {
+    this.loadPsnData();
+  }
+
+  loadPsnData() {
     this.http.get('http://192.168.0.140:5000/api/Person').subscribe(data => {
       // Read the result field from the JSON response.
       this.dataSource = new MatTableDataSource<Person>(data as Person[]);
-      console.log(this.dataSource.data);
+      // console.log(this.dataSource.data);
       this.dataSource.paginator = this.paginator;
     });
+  }
+
+  addPsnData(form) {
+    this.psnData =  {} as Person;
   }
 
   editData(e, that) {
     Object.assign(this.psnData, that);
   }
 
-  saveData() {
-    const myHttpHead = { headers: new HttpHeaders({ 'Content-Type': 'application/json' }) };
-    if (this.psnData && this.psnData['A0101']) {
-      this.http.put('https://easy-mock.com/mock/59c37dc2e0dc663341b35a80/example/proxy', JSON.stringify(this.psnData), myHttpHead)
-      .subscribe(
+  saveData(form) {
+    if (this.psnData && this.psnData['PersonID']) {
+      this.http.put('http://192.168.0.140:5000/api/Person', JSON.stringify(this.psnData), this.myHttpHead).subscribe(
         data => {
+          this.loadPsnData();
           console.log(data);
         },
         err => {
           console.log(err);
         }
       );
+    } else {
+      if (form.value) {
+        this.http.post('http://192.168.0.140:5000/api/Person', JSON.stringify(this.psnData), this.myHttpHead).subscribe(
+          data => {
+            this.loadPsnData();
+            console.log(data);
+          },
+          err => {
+            console.log(err);
+          }
+        );
+      }
     }
+  }
+
+  delData(e, that, form) {
+    const id = that.PersonID;
+    if (id === '') { return; }
+    this.http.delete('http://192.168.0.140:5000/api/Person/' + id, this.myHttpHead).subscribe(
+        data => {
+          this.loadPsnData();
+          console.log(data);
+        },
+        err => {
+          console.log(err);
+        }
+      );
   }
 }
 
