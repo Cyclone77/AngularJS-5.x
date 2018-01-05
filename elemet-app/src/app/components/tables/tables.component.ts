fe64f30d2ad0f123558928e3147f358bc57e06ab
Cyclone77 <fillchar@live.com>
2018-01-05 18:04:00 +0800

    状态管理

--- elemet-app/src/app/components/tables/tables.component.ts
+++ elemet-app/src/app/components/tables/tables.component.ts
@@ -6,24 +6,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tables.component.css']
 })
 export class TablesComponent implements OnInit {
-  private tableData: any[] = [{
-    name: '水爷',
-    date: '2017-08-19',
-    address: '上海市普陀区金沙江路 1518 弄',
-  }, {
-    name: '水爷',
-    date: '2017-08-20',
-    address: '上海市普陀区金沙江路 1518 弄',
-  }, {
-    name: '水爷',
-    date: '2017-08-21',
-    address: '上海市普陀区金沙江路 1518 弄',
-  }, {
-    name: '水爷',
-    date: '2017-08-22',
-    address: '上海市普陀区金沙江路 1510 弄',
-  }];
-  constructor() { }
+  tableData: any[] = [];
+  constructor() {
+    setTimeout(() => {
+      this.tableData = [{
+        name: '水爷',
+        date: '2017-08-19',
+        address: '上海市普陀区金沙江路 1518 弄',
+      }, {
+        name: '水爷',
+        date: '2017-08-20',
+        address: '上海市普陀区金沙江路 1518 弄',
+      }, {
+        name: '水爷',
+        date: '2017-08-21',
+        address: '上海市普陀区金沙江路 1518 弄',
+      }, {
+        name: '水爷',
+        date: '2017-08-22',
+        address: '上海市普陀区金沙江路 1510 弄',
+      }];
+    }, 100);
+  }
 
   ngOnInit() {
   }
